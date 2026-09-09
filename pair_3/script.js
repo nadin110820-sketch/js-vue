c4d105065d68e9bbda015dd28c56504d79eac3dd
Nadia Lynovitskaya <nadin110820@gmail.com>
2026-09-09 11:12:52 +0300

    Add files via upload

---
+++ pair_3/script.js
@@ -0,0 +1,62 @@
+// let age = prompt("Enter your age");
+// let access = confirm("Are you registered?");
+// if(age >= 18 && access === true){
+//     alert("WELCOME!")
+// }else{
+//     alert("Access denied")
+// }
+
+// let accessType = prompt("What is your access type?");
+// if(accessType == "admin"){
+//     alert("Welcome, admin!");
+// }else{
+//     alert("Denied");
+// }
+
+// let login = confirm("Are you registered");
+// let age = Number(prompt("What is your age"));
+// if(login === true){
+//     if(age >= 18){
+//         alert("Ok")
+//     }else{
+//         alert("No")
+//     }
+// }
+
+// let age = Number(prompt("Enter your age"));
+// if(age >= 18){
+//     alert("dorosliy")
+// }else if (age >= 14){
+//     alert("pidlitok")
+// }else if(age >= 6){
+//     alert("shkolota")
+// }else{
+//     alert("ditina")
+// }
+
+// let age = Number(prompt("Enter your age"));
+// if(age >= 18){
+//     alert("Access granted")
+// }else if(age < 18 && age >= 16){
+//     let access = confirm("Do you have permission?");
+//     if(access === true){
+//         alert("Access granted")
+//     }else{
+//         alert("no")
+//     }
+// }
+
+// let name = prompt('NAME');
+// let price = prompt("price");
+// let quantity = prompt("Quantity");
+// let sign = confirm("Are tou signed in");
+// let promo = prompt("Have promo?");
+// const promo2 = "sale";
+// let VIP = confirm("status");
+// const sale1 = 0.1;
+
+// if(price*quantity >= 1000 && sign === true && (promo === promo2 || VIP === true)){
+//     alert(`Price with sale:${price*quantity*sale1}`)
+// }else{
+//     alert("tralalal")
+// }
